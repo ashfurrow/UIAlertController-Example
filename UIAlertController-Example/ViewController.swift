fee1febada354a3972e91fdac0b40cbcbd394fe4
Ash Furrow <ash@ashfurrow.com>
2014-09-07 19:16:25 +0200

    Simplification.

--- UIAlertController-Example/ViewController.swift
+++ UIAlertController-Example/ViewController.swift
@@ -11,21 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBAction func buttonWasPressed(sender: UIButton) {
         var alertViewControllerTextField: UITextField?
-        let handler = { () -> () in
-            // Probably not necessary to unwrap the optional in this way, since the alertViewControllerTextField will be populated immediately
-            if let alertViewControllerTextField = alertViewControllerTextField {
-                // Do whatever you want with the text now
-                println("\(alertViewControllerTextField.text)")
-            }
-        }
         
         let promptController = UIAlertController(title: "Type Something", message: nil, preferredStyle: .Alert)
         let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
-            handler()
-            promptController.dismissViewControllerAnimated(true, completion: nil)
+            println("\(alertViewControllerTextField?.text)")
         })
         let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
-            promptController.dismissViewControllerAnimated(true, completion: nil)
         }
         promptController.addAction(ok)
         promptController.addAction(cancel)
